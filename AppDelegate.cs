46059394096ed3c095df38e75ac9dfccb7a24215
Charles Yiu <mike.flynn.abbe@gmail.com>
2021-12-02 18:39:34 +0800

    New update

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -4,7 +4,7 @@ using Foundation;
 namespace Number
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public partial class AppDelegate : NSApplicationDelegate
     {
         public AppDelegate()
         {
@@ -14,10 +14,45 @@ namespace Number
         {
             // Insert code here to initialize your application
         }
-
+        // Get current view controller
+        Number GetCurrentViewController()
+        {
+            return ((Number)NSApplication.SharedApplication.KeyWindow.ContentViewController);
+        }
         public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
+        }
+        public void SetChangeButtonTitle(string Text)
+        {
+            // Change title to passed text
+            ChangeButton.Title = Text;
+        }
+        partial void ChangeAction(NSObject sender)
+        {
+            Number CurrentViewController = GetCurrentViewController();
+            // Toggle editing
+            CurrentViewController.SetChangable(!CurrentViewController.Changable);
+        }
+        partial void DeleteAction(NSObject sender)
+        {
+            Number CurrentViewController = GetCurrentViewController();
+            // If changable
+            if (CurrentViewController.Changable)
+            {
+                // set changable to false
+                CurrentViewController.SetChangable(false);
+            }
+            // Reset
+            CurrentViewController.NumberContent.ChangeNumber(0);
+        }
+        partial void CopyAction(NSObject sender)
+        {
+            GetCurrentViewController().NumberContent.CopyNumber();
+        }
+        partial void PasteAction(NSObject sender)
+        {
+            GetCurrentViewController().NumberContent.PasteNumber();
         }
     }
 }
---
+++ AppDelegate.designer.cs
@@ -0,0 +1,37 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Number
+{
+	partial class AppDelegate
+	{
+		[Outlet]
+		AppKit.NSMenuItem ChangeButton { get; set; }
+
+		[Action ("ChangeAction:")]
+		partial void ChangeAction (Foundation.NSObject sender);
+
+		[Action ("CopyAction:")]
+		partial void CopyAction (Foundation.NSObject sender);
+
+		[Action ("DeleteAction:")]
+		partial void DeleteAction (Foundation.NSObject sender);
+
+		[Action ("PasteAction:")]
+		partial void PasteAction (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ChangeButton != null) {
+				ChangeButton.Dispose ();
+				ChangeButton = null;
+			}
+		}
+	}
+}
